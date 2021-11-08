37dca7df7ebb9967a97bf8fbda435d74249d5fee
emma <emma.ronaldo7@gmail.com>
2021-11-08 16:05:04 -0500

    add show details function

--- controllers/animes.js
+++ controllers/animes.js
@@ -1,6 +1,7 @@
 import { Anime } from "../models/anime.js";
 
 import axios from 'axios';
+import { response } from "express";
 
 function animeSearch(req, res) {
 	console.log('user input', req.body.query);
@@ -21,8 +22,30 @@ function animeSearch(req, res) {
 		});
 }
 
+function show(req, res) {
+	// console.log(req.params.mal_id);
+	axios
+	.get(`https://api.jikan.moe/v3/anime/${req.params.mal_id}`)
+	.then((response) => {
+		Anime.findOne({mal_id: response.data.id})
 
+
+		.then(anime => {
+			res.render('animes/show', {
+			title: 'Anime Details',
+			apiResults: response.data,
+			anime,
+			userHasAnime: anime?.owner.some(profile =>profile._id.equals(req.user.profile._id))
+		})
+		});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.redirect('/');
+		});
+}
 
 export { 
   animeSearch,
+	show,
 };
--- routes/animes.js
+++ routes/animes.js
@@ -4,6 +4,10 @@ import { isLoggedIn } from '../middleware/middleware.js';
 
 const router = Router();
 
+//http://localhost:3000/animes/:id POST
+router.get('/:mal_id', isLoggedIn, animesCtrl.show);
+
+//http://localhost:3000/animes/search POST
 router.post('/search', isLoggedIn, animesCtrl.animeSearch);
 
 
