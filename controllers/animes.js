fda6f1c6c91ae04ffd6ae3c3d300f682e6ce5339
emma <emma.ronaldo7@gmail.com>
2021-11-08 11:58:41 -0500

    add results view

--- controllers/animes.js
+++ controllers/animes.js
@@ -5,9 +5,15 @@ import axios from 'axios';
 function animeSearch(req, res) {
 	console.log('user input', req.body.query);
 	axios
-		.get(`https://api.jikan.moe/v3/search/anime?q=${req.body.query}&page=1`)
+		.get(
+			`https://api.jikan.moe/v3/search/anime?q=${req.body.query}&page=1&limit=9`
+		)
 		.then((response) => {
-		console.log('this is response', response.data.results[0]);
+			// console.log('this is response', response.data.results[0]);
+			res.render('animes/results', {
+				title: 'search results',
+				results: response.data.results,
+			});
 		})
 		.catch((err) => {
 			console.log(err);
